4b299de1a0bb08eb96b48b513818303c7453b736
phamhoangdong <39237364+pdong15dth@users.noreply.github.com>
2023-06-16 10:16:29 +0700

    Update BasePrisma.ts

--- src/services/basePrisma/BasePrisma.ts
+++ src/services/basePrisma/BasePrisma.ts
@@ -1,6 +1,17 @@
 
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
-
+let prisma = new PrismaClient()
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+  } else {
+    let globalWithPrisma = global as typeof globalThis & {
+      prisma: PrismaClient;
+    };
+    if (!globalWithPrisma.prisma) {
+      globalWithPrisma.prisma = new PrismaClient();
+    }
+    prisma = globalWithPrisma.prisma;
+  }
+  
 export default prisma
